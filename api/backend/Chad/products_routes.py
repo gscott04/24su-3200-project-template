7dd384fbc1c2b7d5b19b4ad978a6fdb7f49bfaa5
Nubaha Ahsan <ahsan.nu@northeastern.edu>
2024-08-13 15:45:22 -0400

    Adding routes for camp director chad

--- api/backend/Chad/products_routes.py
+++ api/backend/Chad/products_routes.py
@@ -8,162 +8,41 @@ import json
 from backend.db_connection import db
 
 
-products = Blueprint('products', __name__)
+camp_director = Blueprint('camp_director', __name__)
 
-# Get all the products from the database
-@products.route('/products', methods=['GET'])
-def get_products():
-    # get a cursor object from the database
+@camp_director.route('/camp_director', methods=['GET'])
+def understafffed_cabins(): 
     cursor = db.get_db().cursor()
+    the_query = 'SELECT ca.cabinName, ca.cabinID FROM Cabin ca JOIN Camper cam on ca.cabinID = cam.cabinID WHERE ca.StaffID IS NULL GROUP BY ca.cabinName, ca.cabinID;'
+    cursor.execute(the_query)
+    the_Data = cursor.fetchall()
+    the_response = make_response(the_Data)  
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
 
-    # use cursor to query the database for a list of products
-    cursor.execute('SELECT id, product_code, product_name, list_price, category FROM products')
+    return the_response 
 
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
+@camp_director.route('/camp_director/<camperID>', methods=['GET'])
+def guardian_info(camperID):
+    cursor = db.get_db().cursor() 
+    the_query = 'SELECT g.firstName, g.lastName, g.phone, g.email FROM Guardian g JOIN Camper c ON g.guardianID = c.guardianID WHERE c.camperID = camperID;'.format(camperID)
+    cursor.execute(the_query)
+    the_Data = cursor.fetchall()
+    the_response = make_response(the_Data)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    
+    return the_response 
 
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
 
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
-@products.route('/product/<id>', methods=['GET'])
-def get_product_detail (id):
-
-    query = 'SELECT id, product_name, description, list_price, category FROM products WHERE id = ' + str(id)
-    current_app.logger.info(query)
-
+@camp_director.route('camp_director/', methods=['GET'])
+def unpaid_guardians(): 
     cursor = db.get_db().cursor()
-    cursor.execute(query)
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    the_data = cursor.fetchall()
-    for row in the_data:
-        json_data.append(dict(zip(column_headers, row)))
-    return jsonify(json_data)
-    
+    the_query = 'SELECT g.firstName, g.lastName, g.phone FROM Guardian g WHERE g.paid = FALSE ORDER BY g.lastName ASC;'
+    cursor.execute(the_query)
+    the_Data = cursor.fetchall()
+    the_response = make_response(the_Data)
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
 
-# get the top 5 products from the database
-@products.route('/mostExpensive')
-def get_most_pop_products():
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT product_code, product_name, list_price, reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 5
-    '''
-    cursor.execute(query)
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
-
-@products.route('/tenMostExpensive', methods=['GET'])
-def get_10_most_expensive_products():
-    
-    query = '''
-        SELECT product_code, product_name, list_price, reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 10
-    '''
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-    
-    return jsonify(json_data)
-
-@products.route('/product', methods=['POST'])
-def add_new_product():
-    
-    # collecting data from the request object 
-    the_data = request.json
-    current_app.logger.info(the_data)
-
-    #extracting the variable
-    name = the_data['product_name']
-    description = the_data['product_description']
-    price = the_data['product_price']
-    category = the_data['product_category']
-
-    # Constructing the query
-    query = 'insert into products (product_name, description, category, list_price) values ("'
-    query += name + '", "'
-    query += description + '", "'
-    query += category + '", '
-    query += str(price) + ')'
-    current_app.logger.info(query)
-
-    # executing and committing the insert statement 
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-    
-    return 'Success!'
-
-### Get all product categories
-@products.route('/categories', methods = ['GET'])
-def get_all_categories():
-    query = '''
-        SELECT DISTINCT category AS label, category as value
-        FROM products
-        WHERE category IS NOT NULL
-        ORDER BY category
-    '''
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-
-    json_data = []
-    # fetch all the column headers and then all the data from the cursor
-    column_headers = [x[0] for x in cursor.description]
-    theData = cursor.fetchall()
-    # zip headers and data together into dictionary and then append to json data dict.
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-    
-    return jsonify(json_data)
-
-@products.route('/product', methods = ['PUT'])
-def update_product():
-    product_info = request.json
-    current_app.logger.info(product_info)
-
-    return "Success"
+    return the_response
