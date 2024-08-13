6179724093fe8fdd1d40c3fab0723afb7f56bce6
lynna-t <lynna.truong3@gmail.com>
2024-08-13 15:05:02 -0400

    added guardian daily schedule in pages

---
+++ app/src/pages/111_Guardian_DailySchedule.py
@@ -0,0 +1,17 @@
+import logging
+logger = logging.getLogger(__name__)
+import streamlit as st
+import requests
+from streamlit_extras.app_logo import add_logo
+from modules.nav import SideBarLinks
+
+SideBarLinks()
+
+st.write("# Find your camper's daily schedule!")
+
+guardian = requests.get('http://api:4000/g/guardian/<c_date>').json()
+
+try:
+  st.dataframe(guardian)
+except:
+  st.write("Could not connect to database to get guardian!")
