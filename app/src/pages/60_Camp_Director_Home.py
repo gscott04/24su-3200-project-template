45e4dd390fe10739dad0fd42e2641e25cd617322
Gillian Scott <gscott@learningexplorer.com>
2024-08-14 14:18:33 -0400

    adding cabin director cabin info pages

--- app/src/pages/60_Camp_Director_Home.py
+++ app/src/pages/60_Camp_Director_Home.py
@@ -14,10 +14,10 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('Get Cabin Information', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/11_Prediction.py')
+  st.switch_page('pages/61_Camp_Director_Cabin_Info.py')
 
 if st.button('View the Simple API Demo', 
              type='primary',
