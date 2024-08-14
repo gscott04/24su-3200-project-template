e79ffaab3d8bdc6885295f288003f0a2cd543e79
Gillian Scott <gscott@learningexplorer.com>
2024-08-14 15:36:36 -0400

    worked on 61, teied to make it allow for no cabin name

--- app/src/pages/61_Camp_Director_Cabin_Info.py
+++ app/src/pages/61_Camp_Director_Cabin_Info.py
@@ -7,28 +7,31 @@ from modules.nav import SideBarLinks
 
 SideBarLinks()
 
-st.write("View Cabin Information")
+st.write("View Cabin Information for Staff")
 
-cabinID =st.number_input("Enter the cabin you want to get information from", step=1)
+staffID =st.number_input("Enter the staff ID to find the cabin they're staying in", step=1)
 
-if st.button('Get Cabin IDs', type='primary', use_container_width=True):
+if st.button('Get Cabin Info', type='primary', use_container_width=True):
     try:
-        url = f'http://api:4000/d/camp_director/{cabinID}'
+        url = f'http://api:4000/d/camp_director/{staffID}'
         response = requests.get(url)
         
         if response.status_code == 200:
             cabin_data = response.json()
-            st.write(f"Cabin IDs in the camp (Cabin ID: {cabinID})")
+            st.write(f"Cabin info for the staff member (Staff ID: {staffID})")
             if cabin_data:
                 for item in cabin_data:
                     if 'cabinID' in item:
-                        st.write(f"Cabin Name: {item['cabinName']}")
+                        if item['cabinName'] is None:
+                            st.write(f"Cabin ID: {item['cabinID']}")
+                        if item['cabinName'] is not None:
+                            st.write(f"Cabin Name: {item['cabinName']}, Cabin ID: {item['cabinID']}")
                     else:
                         st.write(f"Unexpected item structure: {item}")
             else:
-                st.write("No cabin information available for your camp.")
+                st.write("No cabin information available for that staff member.")
         else:
-            st.error(f"Failed to fetch director info. Status code: {response.status_code}")
+            st.error(f"Failed to fetch cabin info. Status code: {response.status_code}")
             st.write("Response Content:")
             st.code(response.text)
     except requests.RequestException as e:
