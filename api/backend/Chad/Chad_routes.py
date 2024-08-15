ec5b61fe4e3b8a4111bebcbde4cba1c557f67425
Nubaha Ahsan <ahsan.nu@northeastern.edu>
2024-08-15 01:18:09 -0400

    comments done on Chad_routes.py

--- api/backend/Chad/Chad_routes.py
+++ api/backend/Chad/Chad_routes.py
@@ -7,36 +7,46 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from backend.db_connection import db
 
-
+# Creating a Blueprint for camp_director routes
 camp_director = Blueprint('camp_director', __name__)
 
 @camp_director.route('/camp_director/<staffID>', methods=['GET'])
-def staff_cabins(staffID): 
+def staff_cabins(staffID):
+    '''Route to get cabins assigned to a staff member by their staffID.''' 
+    # Get a database cursor to execute the query 
     cursor = db.get_db().cursor()
     the_query = f'''
         SELECT ca.cabinName, ca.cabinID 
 	    FROM Cabin ca 
 	    WHERE ca.staffID = {staffID}; 
-
     '''
+    # Execute the SQL query
     cursor.execute(the_query)
+    # Fetching all results from query execution
     the_Data = cursor.fetchall()
+    # Creating a response object with the data
     the_response = make_response(the_Data)  
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
 
     return the_response 
 
+
 @camp_director.route('/camp_director/G/<camperID>', methods=['GET'])
 def guardian_info(camperID):
+    '''Route to get guardian information for a camper by their camperID.'''
+    # Get a database cursor to execute the query
     cursor = db.get_db().cursor() 
     the_query = f'''
     SELECT g.guardianID, g.firstName, g.lastName, g.phone, g.email 
         FROM Guardian g JOIN Camper c ON g.guardianID = c.guardianID 
         WHERE c.camperID = {camperID};
     '''
+    # Execute the SQL query
     cursor.execute(the_query)
+    # Fetching all results from query execution
     the_Data = cursor.fetchall()
+    # Creating a response object with the data
     the_response = make_response(the_Data)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
@@ -46,14 +56,18 @@ def guardian_info(camperID):
 
 @camp_director.route('/camp_director/unpaid', methods=['GET'])
 def unpaid_guardians(): 
+    '''Route to get all unpaid guardians.'''
+    # Get a database cursor 
     cursor = db.get_db().cursor()
     the_query = '''SELECT g.firstName, g.lastName, g.phone FROM Guardian g WHERE g.paid = 0 ORDER BY g.lastName ASC;
     '''
+    # Execute the SQL query
     cursor.execute(the_query)
+    # Fetching all results from query execution
     the_Data = cursor.fetchall()
+    # Creating a response object with the data
     the_response = make_response(the_Data)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
 
     return the_response
-
