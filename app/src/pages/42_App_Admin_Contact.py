0ece9247d4eb0b0b803ffebb2bb84652e630ea75
Nubaha Ahsan <ahsan.nu@northeastern.edu>
2024-08-15 13:16:14 -0400

    deleted northwind sql files.

--- app/src/pages/42_App_Admin_Contact.py
+++ app/src/pages/42_App_Admin_Contact.py
@@ -13,20 +13,23 @@ st.write("Find a guardian's contact information")
 
 # Input field for adminID
 adminID = st.number_input("Enter your ID number 2390", step=1)
-
+# Button to get the contact info for guardians based on the adminID
 if st.button('Get Contact Info', type='primary', use_container_width=True):
     try:
+        # Send a GET request to the API with the adminID
         url = f'http://api:4000/a/app_admin/{adminID}'
-        
         response = requests.get(url)
-        
+        # Parse the response as JSON from the API
         if response.status_code == 200:
             try:
                 guardian_contact = response.json()
+                # Display guardian contact information 
                 st.write(f"Contact info for guardians based on your ID: {adminID}")
                 if guardian_contact:
+                    # Loop through the guardian contact data and display the contact information
                     for item in guardian_contact:
                         st.write(f"Guardian Phone: {item['phone']}, Guardian Email: {item['email']}")
+                # Error messages
                 else:
                     st.write(f"No contact information available for your camps.")
             except ValueError as json_error:
